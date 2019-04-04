d36b42bb47250c59ce5d32360f30926966d4995f
yuanlijianvip <yuanlijianvip@163.com>
2019-04-04 21:39:06 +0800

    项目初始化

---
+++ shop/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+      <ul class="footer-bar">
+        <li class="footer-bar-item">首页</li>
+        <li class="footer-bar-item">分类</li>
+        <li class="footer-bar-item">购物车</li>
+        <li class="footer-bar-item">我的</li>
+      </ul>
+  </div>
+</template>
+
+<style lang="scss">
+.footer-bar{
+  display:flex;
+  background-color: #eeeeee;
+  position:fixed;
+  bottom: 0;
+  width: 100%;
+  &-item{
+    flex-grow:1;
+    text-align:center;
+  }
+}
+</style>
