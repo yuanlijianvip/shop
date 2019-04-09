2cfb8ef30ead8551f3ba2e7f29213137638b3f9c
yuanlijianvip <yuanlijianvip@163.com>
2019-04-09 19:38:50 +0800

    注册加盐加密

--- shop_server/model/User.js
+++ shop_server/model/User.js
@@ -1,11 +1,24 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
+const bcrypt=require('bcrypt');
 
 const userSchema=new Schema({
     userId:Schema.Types.ObjectId,
     userName:{unique:true,type:String},
     password:String,
     createDate:{type:Date,default:Date.now()}
+});
+
+userSchema.pre('save',function(next){
+    //随机生成salt  10迭代次数
+    bcrypt.genSalt(10,(err,salt)=>{
+        if(err) return next(err);
+        bcrypt.hash(this.password,salt,(err,hash)=>{
+            if(err) return next(err)
+            this.password=hash;
+            next();
+        })
+    });
 });
 
 //发布模型
